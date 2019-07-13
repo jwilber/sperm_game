87ad100b93abdda121bf036774029db1aee1176a
Jared Wilber <j.wilber92@gmail.com>
2019-07-13 16:26:30 -0700

    update

--- spermgame1.js
+++ spermgame1.js
@@ -4,14 +4,14 @@ let label = '';
 let spring = 0.025;
 let mainSperm;
 let sperm1, sperm2;
-let jareds = [];
+let sperms = [];
 
 let video;
 let features;
 let knn;
 let labelP;
 let ready = false;
-let numImgs = 100;
+let numImgs = 250;
 
 function preload() {
 	sperm2 = loadImage('newsperm2.png');
@@ -20,21 +20,23 @@ function preload() {
 
 
 function setup() {
-	createCanvas(600, 400);
+	let gameCanvas = createCanvas(800, 400);
+	gameCanvas.parent("game-container");
 	video = createCapture(VIDEO);
 	video.size(600, 400);
+	video.parent('video-container');
 
 	// main sperm image
 	let imgArray = duplicateElements([sperm2], numImgs);
 	console.log(imgArray);
 	for (let i = 0; i < numImgs; i++) {
-          jareds[i] = new SpermCell(
+          sperms[i] = new SpermCell(
             imgArray[i],
             random(width),
             random(height),
             random(30, 70),
             i,
-            jareds
+            sperms
           );
         }
 
@@ -96,14 +98,14 @@ function modelReady() {
 
 
 function draw() {
-	background('white');
+	background('#ffd9df');
 	console.log(label)
-	jareds.forEach(jared => {
+	sperms.forEach(jared => {
         jared.collide();
         jared.move();
         jared.display();
       });
-	image(mainSperm, x, y, mainSperm.width / 14, mainSperm.height / 14);
+	image(mainSperm, x, y, 40, 80);
 
 	// if (label == 'left') {
 	// 	if (x > 0) {
@@ -125,19 +127,19 @@ function draw() {
 	// }
 	if (keyIsDown(LEFT_ARROW)) {
 		if ( x > 0) {
-	    x = x - 4;			
+	    x = x - 6;			
 		}
   } else if (keyIsDown(RIGHT_ARROW)) {
     if (x < width - 40) {
-	    x = x + 4;
+	    x = x + 6;
     }
   } else if (keyIsDown(UP_ARROW)) {
   	if (y > 3) {
-	  	y = y - 4;  		
+	  	y = y - 6;  		
   	}
   } else if (keyIsDown(DOWN_ARROW)) {
   	if (y < height - 55) {
-	   y = y + 4;
+	   y = y + 6;
 		}
   }
 
@@ -152,57 +154,75 @@ class SpermCell {
     this.img = img;
     this.x = xIndex;
     this.y = yIndex;
-    this.xVelocity = 1.5;
+    this.xVelocity = .5;
     this.yVelocity = 1.5;
     this.diameter = 25;
     this.id = idin;
     this.others = oin;
+    this.dead = false;
   }
 
   collide() {
-    for (let i = this.id + 1; i < numImgs; i++) {
-      let dx = this.others[i].x - this.x;
-      let dy = this.others[i].y - this.y;
-      let distance = sqrt(dx * dx + dy * dy);
-      let minDist = this.others[i].diameter / 2 + this.diameter / 2;
-      if (distance < minDist) {
-        let angle = atan2(dy, dx);
-        let targetX = this.x + cos(angle) * minDist;
-        let targetY = this.y + sin(angle) * minDist;
-        let ax = (targetX - this.others[i].x) * spring;
-        let ay = (targetY - this.others[i].y) * spring;
-        this.xVelocity -= ax;
-        this.yVelocity -= ay;
-        this.others[i].xVelocity += ax;
-        this.others[i].yVelocity += ay;
-      }
-    }
+  	if (this.dead === true) {
+  		// do nothing
+  	} else {
+	    for (let i = this.id + 1; i < numImgs; i++) {
+	      let dx = this.others[i].x - this.x;
+	      let dy = this.others[i].y - this.y;
+	      let distance = sqrt(dx * dx + dy * dy);
+	      let minDist = this.others[i].diameter / 2 + this.diameter / 2;
+	      if (distance < minDist) {
+	        let angle = atan2(dy, dx);
+	        let targetX = this.x + cos(angle) * minDist;
+	        let targetY = this.y + sin(angle) * minDist;
+	        let ax = (targetX - this.others[i].x) * spring;
+	        let ay = (targetY - this.others[i].y) * spring;
+	        this.xVelocity -= ax;
+	        this.yVelocity -= ay;
+	        this.others[i].xVelocity += ax;
+	        this.others[i].yVelocity += ay;
+	        console.log(this)
+	        // this.dead = true;
+	      }
+	    }
+  	}
   }
 
   move() {
     // move 
-    this.x += this.xVelocity;
-    this.y += this.yVelocity;
+    if (this.dead === true) {
+    	// pass
+    	// this.x = 0;
+    	// this.y = 0;
+    } else {
+		this.x += this.xVelocity;
+	    this.y += this.yVelocity;
 
-    // take care of edge collisions
-    if (this.x + this.diameter / 2 > width) {
-      this.x = width - this.diameter / 2;
-      this.xVelocity *= -1;
-    } else if (this.x - this.diameter / 2 < 0) {
-      this.x = this.diameter / 2;
-      this.xVelocity *= -1;
-    }
-    if (this.y + this.diameter / 2 > height) {
-      this.y = height - this.diameter / 2;
-      this.yVelocity *= -1;
-    } else if (this.y - this.diameter / 2 < 0) {
-      this.y = this.diameter / 2;
-      this.yVelocity *= -1;
+	    // take care of edge collisions
+	    if (this.x + this.diameter / 2 > width) {
+	      this.x = width - this.diameter / 2;
+	      this.xVelocity *= -1;
+	    } else if (this.x - this.diameter / 2 < 0) {
+	      this.x = this.diameter / 2;
+	      this.xVelocity *= -1;
+	    }
+	    if (this.y + this.diameter / 2 > height) {
+	      this.y = height - this.diameter / 2;
+	      this.yVelocity *= -1;
+	    } else if (this.y < 0) {
+	      this.y = this.diameter / 2;
+	      this.yVelocity *= -1;
+	    }
     }
   }
 
   display() {
-    image(this.img, this.x, this.y, 15, 60);
+  	if (this.dead === true) {
+  		// do nothing
+  		image(this.img, -10, -10, 3, 10);
+  	} else {
+	    image(this.img, this.x, this.y, 7.5, 30);  		
+  	}
   }
 }
 
@@ -214,3 +234,23 @@ function duplicateElements(array, times) {
       return res.concat(Array(times).fill(current));
   }, []);
 }
+
+
+
+// Ensure scrolling only affects canvas, not page
+var keys = {};
+window.addEventListener("keydown",
+    function(e){
+        keys[e.keyCode] = true;
+        switch(e.keyCode){
+            case 37: case 39: case 38:  case 40: // Arrow keys
+            case 32: e.preventDefault(); break; // Space
+            default: break; // do not block other keys
+        }
+    },
+false);
+window.addEventListener('keyup',
+    function(e){
+        keys[e.keyCode] = false;
+    },
+false);
